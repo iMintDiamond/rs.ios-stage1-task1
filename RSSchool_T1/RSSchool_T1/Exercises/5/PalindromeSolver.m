96aca27c150bb923f1bf2112e1ce1301b3b75e5a
iMintDiamond <i.mint.diamond@gmail.com>
2020-03-11 17:33:14 +0300

    feat: complete #5

--- RSSchool_T1/RSSchool_T1/Exercises/5/PalindromeSolver.m
+++ RSSchool_T1/RSSchool_T1/Exercises/5/PalindromeSolver.m
@@ -2,9 +2,91 @@
 
 @implementation PalindromeSolver
 
+Boolean isPalindrome(NSString *string) {
+    NSUInteger leftIndex = 0;
+    NSUInteger rightIndex = string.length -1;
+
+    while(rightIndex > leftIndex){
+        if ([string characterAtIndex:leftIndex] == [string characterAtIndex:rightIndex]) {
+            leftIndex++;
+            rightIndex--;
+        } else {
+            return false;
+        }
+    }
+
+    return true;
+}
+
 // Complete the highestValuePalindrome function below.
 - (NSString *) highestValuePalindrome:(NSString *)s n:(NSNumber *)n k:(NSNumber *)k {
-    return @"";
+    char *string = calloc([s length] +1, 1);
+    strncpy(string, (char *)[s UTF8String], [s length]);
+    
+    long length = [n longValue];
+    long allowedChanges = [k longValue];
+    long differentCharacters = 0;
+
+    for (long i = 0, j = length -1; i < length /2; i++, j--) {
+        if (string[i] != string[j]){
+            differentCharacters++;
+        }
+    }
+    
+    long leftIndex = 0;
+    long rightIndex = [n longValue] - 1;
+
+    if (differentCharacters > allowedChanges){
+        return [[NSString alloc] initWithUTF8String:"-1"];
+    }
+
+    while(rightIndex >= leftIndex) {
+        if (allowedChanges <= 0) {
+            break;
+        }
+
+        if (string[leftIndex] == string[rightIndex]) {
+            if (allowedChanges > 1
+                && (allowedChanges-2) >= differentCharacters
+                && string[leftIndex] != '9'
+            ) {
+                string[leftIndex] = '9';
+                string[rightIndex] = '9';
+                allowedChanges -= 2;
+            }
+        } else {
+            if (allowedChanges > 1
+                && (allowedChanges -2) >= (differentCharacters -1)
+            ) {
+                if (string[leftIndex] != '9') {
+                    string[leftIndex] = '9';
+                    allowedChanges--;
+                }
+                if (string[rightIndex] != '9') {
+                    string[rightIndex] = '9';
+                    allowedChanges--;
+                }
+            } else {
+                if (string[leftIndex] > string[rightIndex]) {
+                    string[rightIndex] = string[leftIndex];
+                } else {
+                    string[leftIndex] = string[rightIndex];
+                }
+                allowedChanges--;
+            }
+            differentCharacters--;
+        }
+        if (leftIndex == rightIndex && allowedChanges > 0) {
+            string[leftIndex] = '9';
+            allowedChanges--;
+        }
+        leftIndex++;
+        rightIndex--;
+    }
+
+    NSString *finalString = [[NSString alloc] initWithUTF8String:string];
+    
+    return isPalindrome(finalString) ? finalString : [[NSString alloc] initWithUTF8String:"-1"];
 }
 
 @end
