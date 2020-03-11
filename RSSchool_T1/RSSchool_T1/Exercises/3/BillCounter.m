45e73fd62d6697d352099242182d6f527179494d
iMintDiamond <i.mint.diamond@gmail.com>
2020-03-11 20:13:21 +0300

    feat: complete #3

--- RSSchool_T1/RSSchool_T1/Exercises/3/BillCounter.m
+++ RSSchool_T1/RSSchool_T1/Exercises/3/BillCounter.m
@@ -4,7 +4,20 @@
 
 // Complete the following fuction
 - (NSString*)compareResultForBill:(NSArray<NSNumber*>*)bill notIncludingElementWithIndex:(NSInteger)index withGivenSum:(NSNumber*)sum {
-    return @"";
+    NSInteger i = 0;
+    NSInteger total = 0;
+    
+    for (NSNumber *partOfBill in bill) {
+        if (i != index) {
+            total += [partOfBill intValue];
+        }
+        i++;
+    }
+    total /= 2;
+    
+    return total == [sum intValue]
+        ? @"Bon Appetit"
+        : [@([sum intValue] - total) stringValue];
 }
 
 @end
